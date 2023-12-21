9154ba12b25ec0b15a7e2bbedf3df06a5d2fa5b9
xan <149614526+xanunderscore@users.noreply.github.com>
2023-12-21 14:52:25 -0500

    i love variables

--- RotationSolver/Updaters/TargetUpdater.cs
+++ RotationSolver/Updaters/TargetUpdater.cs
@@ -538,12 +538,14 @@ internal static partial class TargetUpdater
         if (Svc.ClientState.TerritoryType == 917) {
             // second boss has one of three buffs corresponding to an alliance letter
             // player needs the matching buff
-            var minibossBuff = b.StatusList.Where(x => x.StatusId == 2409 || x.StatusId == 2410 || x.StatusId == 2411).FirstOrDefault();
-            // 2288 -> 2409
-            // 2289 -> 2410
-            // 2290 -> 2411
-            if (minibossBuff != null)
-                return Player.Status.Any(s => s.StatusId == minibossBuff.StatusId - 121);
+            var minibossBuff = b.StatusList.FirstOrDefault(x => x.StatusId == 2409 || x.StatusId == 2410 || x.StatusId == 2411);
+            if (minibossBuff != null) {
+                // 2288 -> 2409
+                // 2289 -> 2410
+                // 2290 -> 2411
+                var playerBuff = minibossBuff.StatusId - 121;
+                return Player.Status.Any(s => s.StatusId == playerBuff);
+            }
         }
 
         return true;
